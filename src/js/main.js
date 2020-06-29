683ee933da324031333313256c3db4fdf3d14c02
9min <gm870711@gmail.com>
2020-06-29 11:32:05 +0900

    버튼 링크 추가

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,85 @@
+Kakao.init("fd05ae8aaf424b5534d131a95e9f5a43");
+
 lightGallery(document.querySelector("#gallery"));
 
-document.querySelector("#shareFacebook").addEventListener("click", () => {
+const $ = (name) => document.querySelector(name);
+
+const addEvent = (name, event, callback) => {
+  $(name).addEventListener(event, callback);
+};
+
+const target = {
+  name: "엠타워컨벤션",
+  lat: 37.3844042,
+  lon: 126.931606,
+  url: "https://joohee0928.github.io/wedding-invitation/",
+};
+const visualImgUrl =
+  "http://www.mtowerwed.co.kr/attachList/upload/main/m20191125122518267.jpg";
+
+addEvent("#shareFacebook", "click", () => {
   var url = "http://www.naver.com";
   window.open(
     "http://facebook.com/sharer/sharer.php?u=" + encodeURIComponent(url),
     "",
     "left=0,top=0,width=650,height=420,personalbar=0,toolbar=0,scrollbars=0,resizable=0"
   );
+});
+
+addEvent("#btnKakaoMap", "click", () => {
+  window.open(
+    "https://m.map.kakao.com/actions/detailMapView?id=20515606&refService=place"
+  );
+});
+
+addEvent("#btnKakaoNavi", "click", () => {
+  Kakao.Navi.start({
+    name: target.name,
+    x: target.lon,
+    y: target.lat,
+    coordType: "wgs84",
+  });
+});
+
+addEvent("#btnNaverMap", "click", () => {
+  window.open(
+    "https://m.place.naver.com/place/34585318/location?subtab=location"
+  );
+});
+
+addEvent("#btnTMap", "click", () => {
+  window.open(
+    `https://apis.openapi.sk.com/tmap/app/routes?appKey=l7xx0cad021f2b204989bff69ad3176bd011&name=${target.name}&lon=${target.lon}&lat=${target.lat}`
+  );
+});
+
+addEvent("#kakaoTalk", "click", () => {
+  Kakao.Link.sendDefault({
+    objectType: "location",
+    address: "경기 안양시 만안구 안양로 104 엠타워컨벤션 6층 파티오볼룸",
+    addressTitle: "엠타워컨벤션",
+    content: {
+      title: "구민♥주희 첩첩장",
+      description: "구민이와 주희의 결혼식에 초대합니다!",
+      imageUrl: visualImgUrl,
+      link: {
+        mobileWebUrl: target.url,
+        webUrl: target.url,
+      },
+    },
+    social: {
+      likeCount: 999,
+      commentCount: 999,
+      sharedCount: 999,
+    },
+    buttons: [
+      {
+        title: "웹으로 보기",
+        link: {
+          mobileWebUrl: target.url,
+          webUrl: target.url,
+        },
+      },
+    ],
+  });
 });
