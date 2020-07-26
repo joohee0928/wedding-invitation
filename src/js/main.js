e1cfabb49f4a04b723a0c4e51b7dca6523469a93
joohee <joohee0928@kakao.com>
2020-07-26 21:07:43 +0900

    prettier 추가

--- src/js/main.js
+++ src/js/main.js
@@ -4,14 +4,14 @@ const addEvent = (target, event, callback) =>
 
 class WeddingInvitation {
   constructor() {
-    this.name = "엠타워컨벤션";
+    this.name = '엠타워컨벤션';
     this.location = {
       lat: 37.3844042,
       lon: 126.931606,
     };
-    this.pageUrl = "https://joohee0928.github.io/wedding-invitation/";
+    this.pageUrl = 'https://joohee0928.github.io/wedding-invitation/';
     this.imgUrl =
-      "http://www.mtowerwed.co.kr/attachList/upload/main/m20191125122518267.jpg";
+      'http://www.mtowerwed.co.kr/attachList/upload/main/m20191125122518267.jpg';
     this.el = null;
 
     this.init();
@@ -21,18 +21,18 @@ class WeddingInvitation {
   }
 
   init = () => {
-    Kakao.init("fd05ae8aaf424b5534d131a95e9f5a43");
+    Kakao.init('fd05ae8aaf424b5534d131a95e9f5a43');
   };
 
   initElements = () => {
     this.el = {
-      dday: $("#dday"),
-      gallery: $("#gallery"),
-      btnKakaoMap: $("#btnKakaoMap"),
-      btnNaverMap: $("#btnNaverMap"),
-      btnTMap: $("#btnTMap"),
-      btnKakaoTalk: $("#btnKakaoTalk"),
-      btnFacebook: $("#btnFacebook"),
+      dday: $('#dday'),
+      gallery: $('#gallery'),
+      btnKakaoMap: $('#btnKakaoMap'),
+      btnNaverMap: $('#btnNaverMap'),
+      btnTMap: $('#btnTMap'),
+      btnKakaoTalk: $('#btnKakaoTalk'),
+      btnFacebook: $('#btnFacebook'),
     };
   };
 
@@ -46,28 +46,28 @@ class WeddingInvitation {
       btnFacebook,
     } = this.el;
     lightGallery(gallery);
-    addEvent(btnKakaoMap, "click", this.onClickKakaoMap);
-    addEvent(btnNaverMap, "click", this.onClickNaverMap);
-    addEvent(btnTMap, "click", this.onClickTMap);
-    addEvent(btnKakaoTalk, "click", this.onClickKakaoTalk);
-    addEvent(btnFacebook, "click", this.onClickShareFacebook);
+    addEvent(btnKakaoMap, 'click', this.onClickKakaoMap);
+    addEvent(btnNaverMap, 'click', this.onClickNaverMap);
+    addEvent(btnTMap, 'click', this.onClickTMap);
+    addEvent(btnKakaoTalk, 'click', this.onClickKakaoTalk);
+    addEvent(btnFacebook, 'click', this.onClickShareFacebook);
   };
 
   dday = () => {
-    const dday = new Date("September 12, 2020 00:00:00");
+    const dday = new Date('September 12, 2020 00:00:00');
     const gap = dday.getTime() - new Date().getTime();
     const date = Math.floor(gap / (1000 * 60 * 60 * 24)) + 1;
-    this.el.dday.innerHTML = date > 0 ? `D-${date}` : "D-Day";
+    this.el.dday.innerHTML = date > 0 ? `D-${date}` : 'D-Day';
   };
 
   onClickKakaoMap = () => {
     window.location.href =
-      "https://m.map.kakao.com/actions/detailMapView?id=20515606&refService=place#none";
+      'https://m.map.kakao.com/actions/detailMapView?id=20515606&refService=place#none';
   };
 
   onClickNaverMap = () => {
     window.location.href =
-      "https://m.place.naver.com/place/34585318/location?subtab=location";
+      'https://m.place.naver.com/place/34585318/location?subtab=location';
   };
 
   onClickTMap = () => {
@@ -76,12 +76,12 @@ class WeddingInvitation {
 
   onClickKakaoTalk = () => {
     Kakao.Link.sendDefault({
-      objectType: "location",
-      address: "경기 안양시 만안구 안양로 104 엠타워컨벤션 6층 파티오볼룸",
-      addressTitle: "엠타워컨벤션",
+      objectType: 'location',
+      address: '경기 안양시 만안구 안양로 104 엠타워컨벤션 6층 파티오볼룸',
+      addressTitle: '엠타워컨벤션',
       content: {
-        title: "구민♥주희 결혼식에 초대합니다!",
-        description: "9/12(토) pm 5:30 안양 엠타워컨벤션",
+        title: '구민♥주희 결혼식에 초대합니다!',
+        description: '9/12(토) pm 5:30 안양 엠타워컨벤션',
         imageUrl: this.imgUrl,
         link: {
           mobileWebUrl: this.pageUrl,
@@ -90,7 +90,7 @@ class WeddingInvitation {
       },
       buttons: [
         {
-          title: "웹으로 보기",
+          title: '웹으로 보기',
           link: {
             mobileWebUrl: this.pageUrl,
             webUrl: this.pageUrl,
@@ -103,8 +103,8 @@ class WeddingInvitation {
   onClickShareFacebook = () => {
     window.open(
       `http://facebook.com/sharer/sharer.php?u=${this.pageUrl}`,
-      "",
-      "left=0,top=0,width=650,height=420,personalbar=0,toolbar=0,scrollbars=0,resizable=0"
+      '',
+      'left=0,top=0,width=650,height=420,personalbar=0,toolbar=0,scrollbars=0,resizable=0'
     );
   };
 }
